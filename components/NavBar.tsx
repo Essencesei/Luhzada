ee3fb7d19d971bfb891f53deb72f7c1dc19888bf
Jonathan Dumlao <dumlao.jonathan.g.5165@gmail.com>
2023-11-06 09:45:24 +0800

    remove search

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,14 +4,7 @@ import React from "react";
 import { getCart } from "@/libs/db/cart";
 import ShoppingCartButton from "./ShoppingCartButton";
 
-const searchProducts = async (formdata: FormData) => {
-  "use server";
-  const searchQuery = formdata.get("searchQuery")?.toString();
 
-  if (searchQuery) {
-    redirect("/search?query=" + searchQuery);
-  }
-};
 
 const NavBar = async () => {
     const cart  = await getCart()
@@ -25,16 +18,7 @@ const NavBar = async () => {
           </Link>
         </div>
         <div className="flex-none gap-2">
-          <form action={searchProducts}>
-            <div className="form-control">
-              <input
-                type="text"
-                name="searchQuery"
-                placeholder="Search"
-                className="input input-bordered w-full"
-              />
-            </div>
-          </form>
+         
           <ShoppingCartButton cart={cart}/>
         </div>
       </div>
